f19d250964ffd1764b78c3f966383d3fe986752f
Dominick Lombardo & Sami Zhang <pair+lombardo-chcg.samizhang@devbootcamp.com>
2016-06-05 15:37:32 -0500

    Adjust d3 js on line graph view

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,10 +2,10 @@ class GraphsController < ApplicationController
 
   def line
     @events = Event.where(event_claimer_id: current_user.id)
-    p @events
+    @events = @events.order(:start_time)
     data = []
     @events.each do |event|
-      data << {"date" => event.start_time.to_date.to_s, "points" => event.points}
+      data << {"date" => event.start_time.to_date.to_s, "points" => event.points.to_i}
     end
     p data
     render json: {data: data}
@@ -17,8 +17,8 @@ class GraphsController < ApplicationController
   end
 
   def pie
-    html = render_to_string 'pie_graph'
-    render json: {html: html}
+
+    render json: {html: true}
   end
 
 end
