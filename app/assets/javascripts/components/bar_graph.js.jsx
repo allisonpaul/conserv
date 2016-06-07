fe31b670c7470e54b8a4c329e467af2c0008a03f
Dominick Lombardo <email": null>
2016-06-07 10:31:28 -0500

    Adjust bar graph js

--- app/assets/javascripts/components/bar_graph.js.jsx
+++ app/assets/javascripts/components/bar_graph.js.jsx
@@ -40,6 +40,7 @@ var BarGraph = React.createClass ({
         .transition().style("width", function(d) { return d * 2 + "%"; })
         .text(function(d, i) { return dates[i] + "  " + points[i] + " points"; }).style("color", "lightblue")
         .transition().text(function(d, i) { return dates[i] + " " + points[i] + " points"; }).style("color", "whitesmoke");
+
   },
 
   render: function(){
